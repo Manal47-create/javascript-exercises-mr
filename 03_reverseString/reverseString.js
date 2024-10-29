03b3f43d41f11488ad598e5822084cc51b5b269e
Manal47-create <[176275083+Manal47-create@users.noreply.github.com]>
2024-10-29 09:29:13 +0100

    reversestringsolu

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+function reverseString(str) {
+    // Convert the string to an array, reverse it, and join it back to a string
+    return str.split('').reverse().join('');
+}
 
-};
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.")
+reverseString("")
 
 // Do not edit below this line
 module.exports = reverseString;
