fa917074d65847fe3774fb809ce25a4e04843cb3
Manal47-create <[176275083+Manal47-create@users.noreply.github.com]>
2024-10-29 09:01:24 +0100

    repeatstringsolution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+function repeatStringNumTimes(string, times) {
+    var repeatString = "";
 
-};
+    if (times < 0) {
+        return "ERROR";
+      }
+
+    while (times > 0) {
+      repeatString += string;
+      times--;
+    }
+    return repeatString;
+  }
+  repeatStringNumTimes("hey", 3);
+  repeatStringNumTimes("hello", 10);
+  repeatStringNumTimes("hi", 1);
+  repeatStringNumTimes("bye", 0);
+  repeatStringNumTimes("goodbye", -1);
+  repeatStringNumTimes("", 10);
+  
+
+
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatStringNumTimes;
